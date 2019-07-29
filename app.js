a6464c2b710094ef3eb22147ca166f78ec6d5679
mukul7199 <mukul7199@gmail.com>
2019-07-29 15:09:53 +0530

    start script

--- app.js
+++ app.js
@@ -5,22 +5,19 @@ const bodyParser = require("body-parser");
 const events = require("./routes/events");
 
 const port = process.env.PORT || 3000;
+const MONGODB_URI =
+  process.env.MONGODB_URI ||
+  "mongodb://mukul8299:mukul123@ds151247.mlab.com:51247/deptwebsite";
 const app = express();
 
 app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.json());
 mongoose.set("useCreateIndex", true);
 
-mongoose.connect(
-  "mongodb://mukul8299:mukul123@ds151247.mlab.com:51247/deptwebsite",
-  {
-    useNewUrlParser: true
-  },
-  err => {
-    if (err) return console.log(err);
-    console.log("Mongoose connected");
-  }
-);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, err => {
+  if (err) return console.log(err);
+  console.log("Mongoose connected");
+});
 
 app.get("/", (req, res) => {
   res.send({
