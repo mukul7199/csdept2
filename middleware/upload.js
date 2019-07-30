d09265d51aad5aa196eb86f96da7edb97ed62939
mukul7199 <mukul7199@gmail.com>
2019-07-30 06:54:28 +0530

    multer upload middleware

---
+++ middleware/upload.js
@@ -0,0 +1,24 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname);
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png")
+      cb(null, true);
+    else cb("Uploaded file is not an image", false);
+  }
+});
+
+module.exports = upload;
