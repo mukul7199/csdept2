c2e78654987a5df44ae67aeba76d8899764a4885
mukul7199 <mukul7199@gmail.com>
2019-09-08 10:29:23 +0530

    calendars thumbnail

--- models/Calendar.js
+++ models/Calendar.js
@@ -2,7 +2,8 @@ var mongoose = require("mongoose");
 
 const CalendarSchema = mongoose.Schema({
   title: { type: Number, trim: true, required: true, unique: true },
-  image: { type: String, required: true }
+  image: { type: String, required: true },
+  thumb: { type: String, required: true }
 });
 
 const Calendar = new mongoose.model("Calendar", CalendarSchema);
--- routes/calendars.js
+++ routes/calendars.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const Calendar = require("../models/Calendar");
 const upload = require("../middleware/upload");
@@ -15,24 +17,43 @@ router.get("/", (req, res) => {
     .catch(e => console.log(e));
 });
 
-router.post("/", upload.single("image"), (req, res) => {
-  const calendar = new Calendar({
-    title: req.body.title,
-    image: req.file.path
-  });
-  calendar
-    .save()
-    .then(calendar => res.send(calendar))
-    .catch(e => res.status(400).send(e));
-});
+router.post(
+  "/",
+  upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "thumb", maxCount: 1 }
+  ]),
+  (req, res) => {
+    // console.log(req.files.image[0].path);
+    // console.log(req.files.thumb[0].path);
+    // res.send("HI");
+    const calendar = new Calendar({
+      title: req.body.title,
+      image: req.files.image[0].path,
+      thumb: req.files.thumb[0].path
+    });
+    calendar
+      .save()
+      .then(calendar => res.send(calendar))
+      .catch(e => res.status(400).send(e));
+  }
+);
 
 router.delete("/:sem", (req, res) => {
   const sem = req.params.sem;
-  Calendar.findOneAndDelete({ title: sem })
-    .then(response => {
-      res.send({ response });
+  Calendar.findOne({ title: sem })
+    .then(calendar => {
+      let image = calendar.image;
+      let thumb = calendar.thumb;
+      var imagePath = path.join(__dirname + `/../${image}`);
+      var thumbPath = path.join(__dirname + `/../${thumb}`);
+      fs.unlinkSync(imagePath);
+      fs.unlinkSync(thumbPath);
+      Calendar.findOneAndDelete({ title: sem }).then(calendar => {
+        res.send(calendar);
+      });
     })
-    .catch(e => console.log(e));
+    .catch(e => res.send(e));
 });
 
 module.exports = router;
