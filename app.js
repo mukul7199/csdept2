bd614d3b43a8f4a5dc5e7561d92fc6fb736f2148
mukul7199 <mukul7199@gmail.com>
2019-09-15 13:48:22 +0530

    access control removed

--- app.js
+++ app.js
@@ -19,11 +19,8 @@ app.use(bodyParser.json());
 mongoose.set("useCreateIndex", true);
 
 app.use(function(req, res, next) {
-  var allowedOrigins = ["http://localhost:3000", "https://localhost:3000"];
   var origin = req.headers.origin;
-  if (allowedOrigins.indexOf(origin) > -1) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
+  res.setHeader("Access-Control-Allow-Origin", origin);
   res.header(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
