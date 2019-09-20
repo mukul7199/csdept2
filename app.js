ce68484e1dfa23012f5182fefc67ae19af28597c
mukul7199 <mukul7199@gmail.com>
2019-09-20 11:28:29 +0530

    origin settings

--- app.js
+++ app.js
@@ -19,8 +19,11 @@ app.use(bodyParser.json());
 mongoose.set("useCreateIndex", true);
 
 app.use(function(req, res, next) {
+  var allowedOrigins = ["http://localhost:3000"];
   var origin = req.headers.origin;
-  res.setHeader("Access-Control-Allow-Origin", origin);
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
   res.header(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
