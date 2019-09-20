9d58c04d5d8920d31da5664705e5529146550779
mukul7199 <mukul7199@gmail.com>
2019-09-20 11:37:25 +0530

    origin settings

--- app.js
+++ app.js
@@ -20,20 +20,20 @@ app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.json());
 mongoose.set("useCreateIndex", true);
 
-app.use(function(req, res, next) {
-  var allowedOrigins = ["http://localhost:3000"];
-  var origin = req.headers.origin;
-  if (allowedOrigins.indexOf(origin) > -1) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  res.header("Access-Control-Allow-Headers", "Content-Type, authorization");
-  res.header("Access-Control-Allow-Credentials", true);
-  return next();
-});
+// app.use(function(req, res, next) {
+//   var allowedOrigins = ["http://localhost:3000"];
+//   var origin = req.headers.origin;
+//   if (allowedOrigins.indexOf(origin) > -1) {
+//     res.setHeader("Access-Control-Allow-Origin", origin);
+//   }
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+//   );
+//   res.header("Access-Control-Allow-Headers", "Content-Type, authorization");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   return next();
+// });
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, err => {
   if (err) return console.log(err);
